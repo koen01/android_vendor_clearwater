455b0306e1dad198cf7f4883738f1f4cfd2967d3
koen01 <koen.erens@gmail.com>
2021-01-23 20:26:36 +0100

    Clearwater vendor initial setup

---
+++ clearwater.mk
@@ -0,0 +1,10 @@
+# Matlog
+PRODUCT_PACKAGES += \
+    MatLog
+
+# Copy some extra permissions
+PRODUCT_COPY_FILES += \
+    vendor/clearwater/permissions/privapp-permissions-clearwater.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-clearwater.xml
+
+
+
