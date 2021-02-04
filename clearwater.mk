5ecd2325232fcdcae7545dad79e43d3d24955fb0
koen01 <koen.erens@gmail.com>
2021-02-04 22:12:10 +0100

    Add faceunlock

--- clearwater.mk
+++ clearwater.mk
@@ -6,5 +6,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/clearwater/permissions/privapp-permissions-clearwater.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-clearwater.xml
 
-
-
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
